6e0b333c1e5a281d259cc00a49055de6425f7b29
kjuq <44694396+kjuq@users.noreply.github.com>
2020-05-27 20:54:03 +0900

    refactored and improved error handling

--- main.go
+++ main.go
@@ -3,81 +3,76 @@ package main
 import (
 	"fmt"
 	"log"
-	"strings"
-	"strconv"
 	"net/http"
+	"strconv"
+	"strings"
+//	"errors"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-const base_url string = "https://smashmate.net/rate/"
-const room_id int = 114157 //72751
+const baseURL string = "https://smashmate.net/rate/"
+const roomId int = 114157 //72751
 
-var url string = base_url + strconv.Itoa(room_id)
+var url string = baseURL + strconv.Itoa(roomId)
 
-func ScrapeMatch() {
+func ScrapeMatch() (error) {
 	fmt.Println(url)
 
 	res, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 	defer res.Body.Close()
 	if res.StatusCode != 200 {
-		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+		return fmt.Errorf("status code error: %d %s", res.StatusCode, res.Status)
 	}
 
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	title := doc.Find(".col-xs-6")
 	title.Each(func(i int, s *goquery.Selection) {
-		character_sel := s.Find(".smash-icon").First()
-		character_img, _ := character_sel.Attr("src")
-		character_splitted := strings.Split(character_img, "/")
-		character_png := character_splitted[len(character_splitted) - 1]
-		character := character_png[:len(character_png) - 4]
+		characterSel := s.Find(".smash-icon").First()
+		characterImg, exists := characterSel.Attr("src")
+		if !exists {
+			return //errors.New("no image src found error")
+		}
+		characterSplitted := strings.Split(characterImg, "/")
+		characterPNG := characterSplitted[len(characterSplitted)-1]
+		character := characterPNG[:len(characterPNG)-4]
 
-		var player_name string
+		var playerName string
 		var rate int
-		//var character = "hoge"
-		var match_status string
+		var matchStatus string
 
 		s.Find(".col-xs-7").Each(func(j int, t *goquery.Selection) {
 			content := strings.TrimSpace(t.Text())
 			switch j {
-			case 0: //name
-				player_name = content
+			case 0: // name
+				playerName = content
 			case 1: // rate
-				rate_str := content
-				rate, _ = strconv.Atoi(rate_str)
+				rateStr := content
+				rate, _ = strconv.Atoi(rateStr)
 			case 2: // fighter
 			case 3: // win or lose
-				match_status = content
+				matchStatus = content
 
 			}
 		})
 
-		/*
-		var isCanceled, isWin bool
-
-		switch match_status {
-		case "対戦中止":
-			isCanceled = true
-		case "勝ち":
-			isWin = true
-		case "負け":
-			isWin = false
-		}
-		*/
-
-		fmt.Println(player_name, rate, character, match_status)
+		fmt.Println(playerName, rate, character, matchStatus)
 	})
+
+	return nil
 }
 
 func main() {
-	ScrapeMatch()
+	if err := ScrapeMatch(); err != nil {
+		fmt.Print("error occered")
+		log.Fatal(err)
+	}
 }
 
